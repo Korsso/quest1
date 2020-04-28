4957127446ff84866b4d9c0b53d529cab4bbf5eb
Valeri <korsso74@gmail.com>
2020-04-28 19:01:00 +0300

    задание2

--- javascript.js
+++ javascript.js
@@ -1,21 +1,22 @@
-let money = prompt ("Ваш бюджет на месяц?"),
-    time = promt ("Введите дату в формате YYYY-MM-DD");
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = promt("Введите дату в формате YYYY-MM-DD", '');
+
 let appData = {
-    buget : money,
-    expenses : {},
-    optionalExpenses : {},
-    income : [],
-    timeData : time, 
+    buget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time, 
     savings: false 
 };
-let a1 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt ("Во сколько обойдется?", ''),
-    a3 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt ("Во сколько обойдется?", '');
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-}  
 
-alert(appData.buget / 30); 
+for (let i = 0; i <2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+
+    appData.expenses[a] = b;
+}
+
+alert(appData.budjet / 30); 
     
     
