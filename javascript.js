a327324375512e722b089a6ed138ea79dc1577ba
Valeri <korsso74@gmail.com>
2020-04-30 19:38:53 +0300

    задание 3.1

--- javascript.js
+++ javascript.js
@@ -1,22 +1,22 @@
-'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money = +prompt ("Ваш бюджет на месяц?", ''),
+    time = prompt ('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    buget: money,
+    budget: money,
+    timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    timeData: time, 
-    savings: false 
+    savings: false
 };
+
 
 for (let i = 0; i <2; i++) {
     let a = prompt ("Ваш бюджет на месяц?", ''),
         b = prompt ('Введите дату в формате YYYY-MM-DD', '');
-        
-        if ( (typeof(a))==='string' && (typeof(a)) != null) && (typeof(b) != null 
+
+        if ( (typeof(a))==='string' && (typeof(a)) != null  && (typeof(b)) != null 
         && a != '' && b != '' && a.length < 50) {
         console.log ("done");
         appData.expenses[a] = b;
@@ -33,7 +33,7 @@ alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
 if(appData.moneyPerDay < 100) {
     console.log ("Минимальный уровень достатка");
- } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000)
+ } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
     console.log ("Средний уровень достатка");
  } else if (appData.moneyPerDay > 2000) {
     console.log ("Высокий уровень достатка");
